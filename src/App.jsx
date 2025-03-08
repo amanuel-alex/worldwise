2afea661463a4edec9e45f27c437f66e4eb2fcbc
amanuel <amanuel2123alex@gmail.com>
2025-03-08 23:20:50 +0300

    components and different pages implementation

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,22 @@
-import { useState } from 'react'
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Products from "./page/Product";
+import Pricing from "./page/Pricing";
+import Homepage from "./page/Homepage";
+import PagesNoFound from "./page/PageNotFound";
+import Login from "./page/Login";
 
 function App() {
-  
-
   return (
-    <>
-    <h1>hello word</h1>
-      
-    </>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="/product" element={<Products />} />
+        <Route path="/pricing" element={<Pricing />} />
+        <Route path="/logint" element={<Login />} />
+        <Route path="*" element={<PagesNoFound />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
